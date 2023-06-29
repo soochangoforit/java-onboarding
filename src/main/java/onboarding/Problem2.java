10b53bf6e9562d90abb759bc9c15ee0cd5ca4c3d
soochan <tncksdl05@gmail.com>
2023-06-29 22:41:29 +0900

    feat(PROBLEM2): 문자열을 한번 순회하면서 중복 제거 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+import java.util.stream.IntStream;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    private static String removeDuplicate(String cryptogram) {
+        StringBuilder result = new StringBuilder();
+        IntStream.range(0, cryptogram.length()).forEach(currentIdx -> {
+            if (!duplicatedWithLeft(cryptogram, currentIdx) && !duplicatedWithRight(cryptogram, currentIdx)) {
+                result.append(cryptogram.charAt(currentIdx));
+            }
+        });
+        return result.toString();
+    }
+
+    private static boolean duplicatedWithRight(String cryptogram, int curIdx) {
+        if (curIdx == cryptogram.length() - 1) {
+            return false;
+        }
+        return cryptogram.charAt(curIdx) == cryptogram.charAt(curIdx + 1);
+    }
+
+    private static boolean duplicatedWithLeft(String cryptogram, int curIdx) {
+        if (curIdx == 0) {
+            return false;
+        }
+        return cryptogram.charAt(curIdx) == cryptogram.charAt(curIdx - 1);
     }
 }
