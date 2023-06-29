a696450f6353ba195e420d1ee78899d2df059eef
soochan <tncksdl05@gmail.com>
2023-06-29 23:13:58 +0900

    refactor(PROBLEM3): 메서드 네이밍과 메서드 단일 책임 준수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,17 +3,33 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int num = 1; num <= number; num++) {
+            answer += getCountOfThreeOrSixOrNine(num);
+        }
         return answer;
     }
 
-    private static int hasThreeOrSixOrNine(int number) {
+    private static int getCountOfThreeOrSixOrNine(int number) {
         int count = 0;
         while(number != 0) {
-            int digit = number % 10;
-            if (digit == 3 || digit == 6 || digit == 9) {
-                count++;
-            }
-            number /= 10;
+            int digit = findFirstDigit(number);
+            count = countValidCondition(count, digit);
+            number = removeFirstDigit(number);
+        }
+        return count;
+    }
+
+    private static int removeFirstDigit(int number) {
+        return number /= 10;
+    }
+
+    private static int findFirstDigit(int number) {
+        return number % 10;
+    }
+
+    private static int countValidCondition(int count, int digit) {
+        if (digit == 3 || digit == 6 || digit == 9) {
+            count++;
         }
         return count;
     }
