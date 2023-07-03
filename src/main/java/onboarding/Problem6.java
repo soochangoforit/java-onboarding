30e6e0f98c6b14b36a8329bd2038141a3c7d5c64
soochan <tncksdl05@gmail.com>
2023-07-03 16:32:49 +0900

    feat(PROBLEM6): 닉네임 유효성 검사

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,8 +3,24 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    private static final String KOREAN_REGEX = "^[가-힣]*$";
+    private static final int MIN_NICKNAME_LENGTH = 1;
+    private static final int MAX_NICKNAME_LENGTH = 20;
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static boolean isValidNickName(String nickName) {
+        return hasOnlyKorean(nickName) && inNickNameLengthRange(nickName);
+    }
+
+    private static boolean inNickNameLengthRange(String nickName) {
+        int nickNameLength = nickName.length();
+        return nickNameLength >= MIN_NICKNAME_LENGTH && nickNameLength < MAX_NICKNAME_LENGTH;
+    }
+
+    private static boolean hasOnlyKorean(String nickName) {
+        return nickName.matches(KOREAN_REGEX);
     }
 }
