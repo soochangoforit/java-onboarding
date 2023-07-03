c863fccaaa399ec11f695cc5a90c861a4c2eb751
soochan <tncksdl05@gmail.com>
2023-07-03 13:10:57 +0900

    feat(PROBLEM4): 알파벳 대소문자 확인 후 변환작업 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,8 +8,23 @@ public class Problem4 {
 
     private static char getGreenFrogChar(char originChar) {
         if(Character.isAlphabetic(originChar)){
-            return originChar;
+            return convertGreenFrogChar(originChar);
         }
         return originChar;
+    }
+
+    private static char convertGreenFrogChar(char originChar) {
+        if(Character.isUpperCase(originChar)){
+            return convertGreenFrogUpperCase(originChar);
+        }
+        return convertGreenFrogLowerCase(originChar);
+    }
+
+    private static char convertGreenFrogLowerCase(char originChar) {
+        return (char) ('a' + ('z' - originChar));
+    }
+
+    private static char convertGreenFrogUpperCase(char originChar) {
+        return (char) ('A' + ('Z' - originChar));
     }
 }
