550ca9ef5ea9ff5e2ac48c8f6a6532d0e43dbc7f
soochan <tncksdl05@gmail.com>
2023-07-03 22:53:46 +0900

    feat(PROBLEM7): 조건에 맞는 결과 출력 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,7 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -13,7 +11,38 @@ public class Problem7 {
         Map<String, Integer> scoreMap = calculateFriendsScore(user, friendsMap);
         plusOnePointToVisitors(visitors, scoreMap);
 
-        return answer;
+        return getTopFive(user, friendsMap, scoreMap);
+    }
+
+    private static List<String> getTopFive(String user, Map<String, List<String>> friendsMap, Map<String, Integer> scoreMap) {
+        return scoreMap.keySet()
+                .stream()
+                .filter(friend -> isNotUser(user, friend))
+                .filter(friend -> isNotUserFriend(user, friendsMap, friend))
+                .sorted(highestScoreFirst(scoreMap))
+                .limit(5)
+                .collect(Collectors.toList());
+    }
+
+    private static Comparator<String> highestScoreFirst(Map<String, Integer> scoreMap) {
+        return (friend1, friend2) -> {
+            if (isSameScore(scoreMap, friend1, friend2)) {
+                return friend1.compareTo(friend2);
+            }
+            return scoreMap.get(friend2) - scoreMap.get(friend1);
+        };
+    }
+
+    private static boolean isSameScore(Map<String, Integer> scoreMap, String friend1, String friend2) {
+        return scoreMap.get(friend1).equals(scoreMap.get(friend2));
+    }
+
+    private static boolean isNotUser(String user, String friend) {
+        return !friend.equals(user);
+    }
+
+    private static boolean isNotUserFriend(String user, Map<String, List<String>> friendsMap, String friend) {
+        return !friendsMap.get(user).contains(friend);
     }
 
     private static void plusOnePointToVisitors(List<String> visitors, Map<String, Integer> scoreMap) {
@@ -27,12 +56,12 @@ public class Problem7 {
         Map<String, Integer> scoreMap = new HashMap<>();
 
         for (String userFriend : friendsMap.get(user)) {
-            plusTenPoint(scoreMap, friendsMap.get(userFriend));
+            plusTenPoint(scoreMap, friendsMap.get(userFriend), user);
         }
         return scoreMap;
     }
 
-    private static void plusTenPoint(Map<String, Integer> scoreMap, List<String> friends) {
+    private static void plusTenPoint(Map<String, Integer> scoreMap, List<String> friends, String mainUser) {
         for(String friend : friends) {
             Integer score = scoreMap.getOrDefault(friend, 0);
             scoreMap.put(friend, score + 10);
@@ -44,8 +73,8 @@ public class Problem7 {
         for (List<String> friend : friends) {
             String friend1 = friend.get(0);
             String friend2 = friend.get(1);
-            friendsMap.computeIfAbsent(friend1, k -> Collections.emptyList()).add(friend2);
-            friendsMap.computeIfAbsent(friend2, k -> Collections.emptyList()).add(friend1);
+            friendsMap.computeIfAbsent(friend1, k -> new ArrayList<>()).add(friend2);
+            friendsMap.computeIfAbsent(friend2, k -> new ArrayList<>()).add(friend1);
         }
         return friendsMap;
     }
