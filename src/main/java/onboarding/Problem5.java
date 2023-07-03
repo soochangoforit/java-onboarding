f7ae3db3a922dbd2fc8873a189539185d081b53f
soochan <tncksdl05@gmail.com>
2023-07-03 15:26:54 +0900

    feat(PROBLEM5): 변환되어야될 지폐/동전이 들어있는 리스트를 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,37 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    enum Money {
+        _50000_won(50000),
+        _10000_won(10000),
+        _5000_won(5000),
+        _1000_won(1000),
+        _500_won(500),
+        _100_won(100),
+        _50_won(50),
+        _10_won(10),
+        _1_won(1);
+
+        private final int value;
+
+        Money(int value) {
+            this.value = value;
+        }
+
+        public static List<Money> highestOrder() {
+            return Arrays.stream(values())
+                    .sorted((o1, o2) -> o2.value - o1.value)
+                    .collect(Collectors.toList());
+        }
     }
 }
