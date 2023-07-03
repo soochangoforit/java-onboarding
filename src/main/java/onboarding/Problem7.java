1a13e9605819f6313b9af8dbc2577b49027a90c2
soochan <tncksdl05@gmail.com>
2023-07-03 22:20:50 +0900

    feat(PROBLEM7): 친구의 친구들에 대해 10점 추가 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,17 +3,35 @@ package onboarding;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
-        HashMap<String, List<String>> friendsMap = friendsMap(friends);
+        Map<String, List<String>> friendsMap = converToFriendMap(friends);
+        Map<String, Integer> scoreMap = calculateFriendsScore(user, friendsMap);
 
         return answer;
     }
 
-    private static HashMap<String, List<String>> friendsMap(List<List<String>> friends) {
+    private static Map<String, Integer> calculateFriendsScore(String user, Map<String, List<String>> friendsMap) {
+        Map<String, Integer> scoreMap = new HashMap<>();
+
+        for (String userFriend : friendsMap.get(user)) {
+            plusTenPoint(scoreMap, friendsMap.get(userFriend));
+        }
+        return scoreMap;
+    }
+
+    private static void plusTenPoint(Map<String, Integer> scoreMap, List<String> friends) {
+        for(String friend : friends) {
+            Integer score = scoreMap.getOrDefault(friend, 0);
+            scoreMap.put(friend, score + 10);
+        }
+    }
+
+    private static HashMap<String, List<String>> converToFriendMap(List<List<String>> friends) {
         HashMap<String, List<String>> friendsMap = new HashMap<>();
         for (List<String> friend : friends) {
             String friend1 = friend.get(0);
