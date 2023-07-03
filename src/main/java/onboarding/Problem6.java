3b276867a927671dfa0130caffcb99f397207f57
soochan <tncksdl05@gmail.com>
2023-07-03 16:42:10 +0900

    feat(PROBLEM6): 이메일 유효성 검사

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,6 +6,9 @@ public class Problem6 {
     private static final String KOREAN_REGEX = "^[가-힣]*$";
     private static final int MIN_NICKNAME_LENGTH = 1;
     private static final int MAX_NICKNAME_LENGTH = 20;
+    private static final String EMAIL_DOMAIN = "@email.com";
+    private static final int MIN_EMAIL_LENGTH = 11;
+    private static final int MAX_EMAIL_LENGTH = 20;
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
@@ -22,5 +25,18 @@ public class Problem6 {
 
     private static boolean hasOnlyKorean(String nickName) {
         return nickName.matches(KOREAN_REGEX);
+    }
+
+    private static boolean isValidEmail(String email) {
+        return endedWithDomain(email) && inEmailLengthRange(email);
+    }
+
+    private static boolean inEmailLengthRange(String email) {
+        int emailLength = email.length();
+        return emailLength >= MIN_EMAIL_LENGTH && emailLength < MAX_EMAIL_LENGTH;
+    }
+
+    private static boolean endedWithDomain(String email) {
+        return email.endsWith(EMAIL_DOMAIN);
     }
 }
